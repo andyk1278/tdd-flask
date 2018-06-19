7cfd0ea73c9d810b1c4a0eee204c7fd7ce4817f2
kimplausible <andyk1278@gmail.com>
2018-06-19 08:34:39 -0700

    created login and logout views and templates

--- app.py
+++ app.py
@@ -14,6 +14,37 @@ PASSWORD = 'admin'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+@app.route('/')
+def show_entries():
+    """Searches the db for entries, then displays them."""
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.hml', entries=entries)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    """User login/authentication/session management."""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    """User logout/authentication/session management"""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
 # connect to db
 def connect_db():
     """Connects to db."""
@@ -40,6 +71,7 @@ def get_db():
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
 
 
 
